dfd79b27db19332246687e5ed0d77b463da55dd9
imkiva <imkiva@islovely.icu>
2021-04-25 22:43:48 +0800

    YML: fuck you vscode

--- build-and-copy.sh
+++ build-and-copy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 ./yaml-to-plist Aya.yml Aya.tmLanguage
+./yaml-to-plist Aya.yml ../aya-prover-vscode/grammars/Aya-embedded.tmLanguage
 ./yaml-to-plist Aya-vscode.yml ../aya-prover-vscode/grammars/Aya.tmLanguage
