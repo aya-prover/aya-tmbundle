f8c938e73eb71586ce1908f1ca8aa2c0906765ec
ice1000 <ice1000kotlin@foxmail.com>
2022-11-17 16:32:25 -0500

    sh: rename project

--- build-and-copy.sh
+++ build-and-copy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ./yaml-to-plist Aya.yml Aya.tmLanguage
-./yaml-to-plist Aya.yml ../aya-prover-vscode/grammars/Aya-embedded.tmLanguage
-./yaml-to-plist Aya-vscode.yml ../aya-prover-vscode/grammars/Aya.tmLanguage
+./yaml-to-plist Aya.yml ../aya-vscode/grammars/Aya-embedded.tmLanguage
+./yaml-to-plist Aya-vscode.yml ../aya-vscode/grammars/Aya.tmLanguage
